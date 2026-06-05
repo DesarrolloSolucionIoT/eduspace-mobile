340d871b13f09f9addf04b5c94b5a252e3660a60
Camila <u202110385@upc.edu.pe>
2026-06-04 19:50:43 -0500

    feat: add new route for initial page.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,6 @@
 import 'package:eduspace_mobile/views/classrooms/ClassroomsPage.dart';
 import 'package:eduspace_mobile/views/iam/LoginPage.dart';
-import 'package:eduspace_mobile/views/iam/RegisterPage.dart';
+import 'package:eduspace_mobile/views/landing/InitialPage.dart';
 import 'package:eduspace_mobile/views/sharedspaces/SharedSpacesPage.dart';
 import 'package:eduspace_mobile/views/teachers/TeachersManagementPage.dart';
 import 'package:flutter/material.dart';
@@ -11,9 +11,9 @@ import '../views/meetings/MeetingsPage.dart';
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => HomePage(),
   '/login': (context) => LoginPage(),
-  '/register': (context) => RegisterPage(),
   '/classrooms': (context) => ClassroomsPage(),
   '/shared-spaces': (context) => SharedSpacesPage(),
   '/meetings': (context) => MeetingsPage(),
-  '/teachers': (context) => TeachersManagementPage()
+  '/teachers': (context) => TeachersManagementPage(),
+  '/welcome' : (context) => InitialPage(),
 };
